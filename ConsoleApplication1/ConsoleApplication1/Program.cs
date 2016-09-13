e4243667a4949ffcbc68b213878fdee55bbbac53
amilaDulanjana <amiladulanjana@hotmail.com>
2016-09-14 00:13:30 +0530

    Add Artist class to EF project

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new MusicContext())
+            {
+                var artist = context.Artists.Count();
+                Console.WriteLine(artist);
+                Console.ReadLine();
+
+            }
+            
+        }
+
+        public class Artist
+        {
+            public int ID { get; set; }
+            public string name { get; set; }
+        }
+
+        public class MusicContext : DbContext
+        {
+            public DbSet<Artist> Artists { get; set; }
+        }
+
+    }
+}
