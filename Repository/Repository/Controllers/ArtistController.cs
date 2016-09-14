2a0576addf3ef1eece9756892e6a81ce18e27e0b
amilaDulanjana <amiladulanjana@hotmail.com>
2016-09-14 23:04:38 +0530

    Artist class and context class are added

---
+++ Repository/Repository/Controllers/ArtistController.cs
@@ -0,0 +1,21 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Repository.Controllers
+{
+    public class ArtistController : Controller
+    {
+
+        MusicStoreDataContext context = new MusicStoreDataContext();
+
+        // GET: Artist
+        public ActionResult Index()
+        {
+            return View(context.Artists.ToList());
+        }
+    }
+}
---
+++ Repository/Repository/Models/MusicStoreDataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Repository.Models
+{
+    public class MusicStoreDataContext : DbContext
+    {
+        public DbSet<Artist> Artists { get; set; }
+    }
+}
